35c3cab47fc255e20a6c1f727bb075902b294e89
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2021-12-12 14:38:18 -0300

    Se agrega interface de about

---
+++ src/app/interfaces/about.interface.ts
@@ -0,0 +1,14 @@
+
+export interface InfoAbout {
+    titulo?: string;
+    nosotros?: boolean;
+    tituloNosotros?: string;
+    parrafoNosotros?: string; 
+    mision?: boolean;
+    tituloMision?: string;
+    parrafoMision?: string; 
+    clientes?: boolean;
+    tituloClientes?: string;
+    parrafoClientes?: string;   
+  }
+  
